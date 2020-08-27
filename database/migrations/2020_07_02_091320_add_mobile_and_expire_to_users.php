dd78209550c290d399ba330c9c664eb524f07c00
uba95 <ubaidaawad@gmail.com>
2020-08-27 10:10:46 +0300

    pgsql

--- database/migrations/2020_07_02_091320_add_mobile_and_expire_to_users.php
+++ database/migrations/2020_07_02_091320_add_mobile_and_expire_to_users.php
@@ -14,7 +14,7 @@ class AddMobileAndExpireToUsers extends Migration
     public function up()
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->string('mobile');
+            $table->string('mobile')->nullable();
             $table->boolean('expire');
         });
     }
